ef541e2bbea913fcdc54c6a8257eeaad5b9f0071
Mohit <mohitdagar2k08@gmail.com>
2025-12-27 13:02:12 +0530

    Implement balanced parentheses checker
    Added a solution for checking balanced parentheses using a stack.

--- Stacks/ValidParentheses.java
+++ Stacks/ValidParentheses.java
@@ -28,3 +28,35 @@ class Solution {
         return false;
     }
 }
+
+
+
+
+
+
+Same Approach just GFG solution
+
+    class Solution {
+    public boolean isBalanced(String s) {
+        Stack<Character> st = new Stack<>();
+        if(s.length() % 2 != 0) return false;
+        for(int i = 0;i<s.length();i++){
+            char ch = s.charAt(i);
+            if(ch=='(' || ch=='[' || ch=='{') st.push(ch);
+            else{
+                if(st.size() == 0) return false;
+                char open = st.peek();
+                if(pair(ch,open)) st.pop();
+                else return false;
+            }
+        }
+        return (st.size()==0); //if size is zero it will return true else false;
+    }
+    public boolean pair(char close,char open){
+        if(open=='(' && close==')') return true;
+        if(open=='[' && close==']') return true;
+        if(open=='{' && close=='}') return true;
+        return false;
+    }
+}
+
