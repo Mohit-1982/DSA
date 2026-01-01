6b5e043be72de40ec8d1f0b06dac835ba2cc44fb
Mohit <mohitdagar2k08@gmail.com>
2026-01-01 18:14:54 +0530

    Create TraverseBT.java

---
+++ Binary Tree/TraverseBT.java
@@ -0,0 +1,29 @@
+class Node{
+    int val;
+    Node left;
+    Node right;
+    Node(int val){
+        this.val = val;
+    }
+}
+public class Implementation {
+    public static void main(String[] args) {
+        Node a = new Node(3);
+        Node b = new Node(4);
+        Node c = new Node(2);
+        Node d = new Node(-1);
+        Node e = new Node(0);
+        Node f = new Node(6);
+        Node g = new Node(9);
+
+        a.left = b;  a.right = c;
+        b.left = d;  b.right = e;
+        c.left = f;  c.right = g;
+    }
+}
+static void dispaly(Node root) {
+        if(root == null) return;
+        System.out.print(root.val + " ");
+        dispaly(root.left);
+        dispaly(root.right);
+    }
