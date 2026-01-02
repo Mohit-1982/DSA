2cbff75029afd61448e7abaa065b80ee321008a9
Mohit <mohitdagar2k08@gmail.com>
2026-01-02 10:55:25 +0530

    Update InvertBT.java

--- Binary Tree/InvertBT.java
+++ Binary Tree/InvertBT.java
@@ -1,5 +1,11 @@
 Leetcode - 226
 
+We change the left reference to point to what was on the right
+Because of that, the left reference no longer points to the old left
+But before breaking that connection, we saved the old left in temp
+Then we attach that saved left to the right side using right = temp
+
+  
   class Solution {
     public TreeNode invertTree(TreeNode root) {
         helper(root);
