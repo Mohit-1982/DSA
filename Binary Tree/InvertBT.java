3aadafe3d538911a755fd0b44a3b85ddf4d1ee31
Mohit <mohitdagar2k08@gmail.com>
2026-01-02 10:53:52 +0530

    Create InvertBT.java

---
+++ Binary Tree/InvertBT.java
@@ -0,0 +1,16 @@
+Leetcode - 226
+
+  class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        helper(root);
+        return root;
+    }
+    static void helper(TreeNode root) {
+        if(root == null) return;
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        helper(root.left);
+        helper(root.right);
+    }
+}
