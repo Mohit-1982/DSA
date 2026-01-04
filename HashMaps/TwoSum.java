4052ab549e88610521b92b586ec7792209869b38
Mohit <mohitdagar2k08@gmail.com>
2026-01-04 18:51:06 +0530

    Create TwoSum.java

---
+++ HashMaps/TwoSum.java
@@ -0,0 +1,10 @@
+class Solution {
+    boolean twoSum(int arr[], int target) {
+        HashSet<Integer> set = new HashSet<>();
+        for(int i = 0; i < arr.length; i++) {
+            if(set.contains(target - arr[i])) return true;
+            set.add(arr[i]);
+        }
+        return false;
+    }
+}
