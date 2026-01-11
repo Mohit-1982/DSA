7db325f05109f81a6fae5d863a189ab25589a576
Mohit <mohitdagar2k08@gmail.com>
2026-01-11 09:46:46 +0530

    Add second approach to set the rightmost bit

--- BitManipulation/SetTheRightMostBit.java
+++ BitManipulation/SetTheRightMostBit.java
@@ -2,4 +2,12 @@ class Solution {
     static int setBit(int n) {
         return n | (n+1);
     }
+    static int setBitApproach2(int n) {
+        for(int i = 0;i<31;i++) {
+            if((n>>i) % 2 == 0){//this bit is 0 we have to set it
+                return n | (1 << i);
+            }
+        }
+        return 403802;
+    }
 }
