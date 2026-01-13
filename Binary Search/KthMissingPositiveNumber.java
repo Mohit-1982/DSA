2feb1a3298a00b29032f241fb39d972e8bfd396f
Mohit <mohitdagar2k08@gmail.com>
2026-01-13 15:20:40 +0530

    Create KthMissingPositiveNumber.java

---
+++ Binary Search/KthMissingPositiveNumber.java
@@ -0,0 +1,17 @@
+Leetcode - 1539
+  class Solution {
+    public int findKthPositive(int[] arr, int k) {
+        int s = 0;
+        int e = arr.length - 1;
+        while(s <= e) {
+            int m = s + (e - s) / 2;
+            int missing = arr[m] - (m + 1);
+            if(missing >= k) {
+                e = m - 1;
+            }else {
+                s = m + 1;
+            }
+        }
+        return e + k + 1;
+    }
+}
