2c68d1973408d9293c16bc2f7ca0826f57f240ac
Mohit <mohitdagar2k08@gmail.com>
2026-01-22 17:31:39 +0530

    Update PrintEncodings.java

--- Recursion/PrintEncodings.java
+++ Recursion/PrintEncodings.java
@@ -20,3 +20,52 @@ public class PrintEncodings {
         }
     }
 }
+
+//Count Encodings
+
+public class CountEncodings {
+    public static void main(String[] args) {
+        String[] arr = {"","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
+        System.out.println(encodings("123",arr,0));
+    }
+        public int encodingsLittleOptimal(String str) {
+            if(str.isEmpty()) {
+                return 1;
+            }
+            if(str.charAt(0) == '0') {
+                return 0;
+            }
+            int one = encodingsLittleOptimal(str.substring(1));
+            int two = 0;
+            if(str.length() >= 2) {
+                String sh = "" + str.charAt(0) + str.charAt(1);
+                int idx1 = (str.charAt(0) - '0') * 10 + (str.charAt(1) - '0');
+                if(idx1 <= 26) {
+                    two = encodingsLittleOptimal(str.substring(2));
+                }
+            }
+            return one + two;
+        }
+        
+    static int encodings(String str,String[] arr,int count) {
+        if(str.isEmpty()) {
+            return 1;
+        }
+        if(str.charAt(0) == '0') {
+            return 0;
+        }
+        int idx = Integer.parseInt("" + str.charAt(0));
+        int one = count + encodings(str.substring(1),arr,count);
+        int two = 0;
+        if(str.length() >= 2) {
+            String sh = "" + str.charAt(0) + str.charAt(1);
+            int idx1 = Integer.parseInt(sh);
+            if(idx1 <= 26) {
+                two = count + encodings(str.substring(2), arr, count);
+            }
+        }
+        return one + two;
+    }
+}
+
+
