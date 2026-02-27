df71d938058665baab53df489808b1b237e50bfe
Mohit <mohitdagar2k08@gmail.com>
2026-02-27 18:47:10 +0530

    Update SticklerThief.java

--- Dynamic Programming/SticklerThief.java
+++ Dynamic Programming/SticklerThief.java
@@ -30,6 +30,7 @@ More Readable :
 }
 
 Optimal : 
+  *Memoization
   class Solution {
     public int findMaxSum(int arr[]) {
         int[] dp = new int[arr.length];
@@ -44,5 +45,18 @@ Optimal :
         int ans = Math.max(pick, skip);
         dp[i] = ans;
         return ans;
+    }
+}
+
+*Tabulation
+    class Solution {
+    public int rob(int[] nums) {
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        if(nums.length > 1) dp[1] = Math.max(nums[0], nums[1]);
+        for(int i = 2; i < nums.length; i++) {
+            dp[i] = Math.max(nums[i] + dp[i - 2], dp[i - 1]);
+        }
+        return dp[nums.length - 1];
     }
 }
