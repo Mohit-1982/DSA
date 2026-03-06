19f8d80b949e505ca1134ecf53b15695494be0fd
Mohit <mohitdagar2k08@gmail.com>
2026-03-06 10:55:53 +0530

    Create DeArrangementOfBalls.java

---
+++ Dynamic Programming/DeArrangementOfBalls.java
@@ -0,0 +1,8 @@
+GFG
+  class Solution {
+    static int countDer(int n) {
+        if(n == 1) return 0;
+        if(n == 2) return 1;
+        return (n - 1) * (countDer(n - 2) + countDer(n - 1));
+    }
+}
