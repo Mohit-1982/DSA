9aeb1bb97f076191582344063415daba25fc337e
Mohit <mohitdagar2k08@gmail.com>
2026-03-20 17:40:43 +0530

    Update LongestCommonSubsequence.java

--- Dynamic Programming/String/LongestCommonSubsequence.java
+++ Dynamic Programming/String/LongestCommonSubsequence.java
@@ -109,4 +109,22 @@ Optimal :
         return dp[n][m];
     }
 }
-  
+    
+  class Solution {
+    public int longestCommonSubsequence(String s, String str) {
+        //Tabulation
+        int n = s.length();
+        int m = str.length();
+        int[][] dp = new int[n + 1][m + 1];
+        for(int i = 1; i < dp.length; i++) {
+            for(int j = 1; j < dp[0].length; j++) {
+                if(s.charAt(i - 1) == str.charAt(j - 1)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                }else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[n][m];
+    }
+}
