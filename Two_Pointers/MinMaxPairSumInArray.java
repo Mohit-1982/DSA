cb1ee47abb029f82a56e14ebdee5081a1730e25f
Mohit <mohitdagar2k08@gmail.com>
2026-03-29 17:44:31 +0530

    Add solution for minimum pair sum problem

---
+++ Two_Pointers/MinMaxPairSumInArray.java
@@ -0,0 +1,15 @@
+Leetcode - 1877
+  class Solution {
+    public int minPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int i = 0;
+        int j = nums.length -1;
+        int res = Integer.MIN_VALUE;
+        while(i < j) {
+            res = Math.max(res, nums[i] + nums[j]);
+            i++;
+            j--;
+        }
+        return res;
+    }
+}
