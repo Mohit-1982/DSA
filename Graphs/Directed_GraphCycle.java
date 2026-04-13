fb91459327a2efa92b77ed0cb388c8d41a2d9f93
Mohit <mohitdagar2k08@gmail.com>
2026-04-13 19:33:36 +0530

    Add cycle detection using Kahn's Algorithm
    Implemented Kahn's Algorithm for cycle detection in directed graphs using BFS. Added methods for adjacency list formation and cycle checking.

--- Graphs/Directed_GraphCycle.java
+++ Graphs/Directed_GraphCycle.java
@@ -47,3 +47,62 @@ GFG
         return adj;
     }
 }
+
+*BFS --> Kahn's Algorithm
+  class Solution {
+    public boolean isCyclic(int V, int[][] edges) {
+        //Kahn's Algorithm BFS
+        ArrayList<ArrayList<Integer>> adj = adjForm(edges, V);
+        int[] indeg = new int[V];
+        
+        for (int i = 0; i < adj.size(); i++) {
+            for (int j = 0; j < adj.get(i).size(); j++) {
+               indeg[adj.get(i).get(j)]++; 
+            }
+        }
+        
+        Queue<Integer> q = new LinkedList<>();
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        for (int i = 0; i < V; i++) {
+            if (indeg[i] == 0) {
+                list.add(i);
+                q.add(i);
+            }
+        }
+        
+        while (!q.isEmpty()) {
+            int front = q.remove();
+            
+            for(int node : adj.get(front)) {
+                indeg[node]--;
+                
+                if (indeg[node] == 0) {
+                    list.add(node);
+                    q.add(node);
+                }
+            }
+        }
+        
+        return (list.size() == V) ? false : true;
+    }
+    
+    
+    public ArrayList<ArrayList<Integer>> adjForm(int[][] arr, int V) {
+        
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        
+        for(int i = 0; i < V; i++) {
+            adj.add(new ArrayList<>());
+        }
+        
+        for(int i = 0; i < arr.length; i++) {
+            int u = arr[i][0];
+            int v = arr[i][1];
+            
+            adj.get(u).add(v);
+        }
+        
+        return adj;
+    }
+}
