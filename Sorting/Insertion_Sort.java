638c2547a4ed6a6c4cab6a67d7015fd45d174c49
Mohit <mohitdagar2k08@gmail.com>
2026-04-18 10:13:32 +0530

    Create Insertion_Sort.java

---
+++ Sorting/Insertion_Sort.java
@@ -0,0 +1,18 @@
+GFG
+  class Solution {
+    public void insertionSort(int arr[]) {
+        int n = arr.length;
+        
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                //shift towards right
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            
+            arr[j + 1] = key;
+        }
+    }
+}
